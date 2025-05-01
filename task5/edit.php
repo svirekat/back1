5b62922e6f6079f497d7e6cf160d7fddbaf4323f
svirekat <eketerinasv@gmail.com>
2025-05-01 09:41:07 +0300

    Update edit.php

--- task5/edit.php
+++ task5/edit.php
@@ -5,8 +5,7 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php"); // Если не авторизован, перенаправляем на вход
     exit();
 }
-// Здесь код для получения данных пользователя из базы данных и их отображения
-// Например:
+
 $user_id = $_SESSION['user_id'];
 $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = ?");
 $stmt->execute([$user_id]);
