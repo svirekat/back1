708c4c215cdbb032db08d8945df70fcda2d4eaaa
svirekat <eketerinasv@gmail.com>
2025-05-01 10:10:14 +0300

    Update login.php

--- task5/login.php
+++ task5/login.php
@@ -1,13 +1,7 @@
 <?php
-session_start(); // Запускаем сессию
-// Подключение к базе данных
-$user = 'u68857'; 
-$password = '9940611'; 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=u68857', $user, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (PDOException $e) {
-    die("<p style='color:red;'>Ошибка подключения к базе данных: " . $e->getMessage() . "</p>");
-}
+session_start(); 
+require_once 'db_connection.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login = $_POST['login'];
     $password = $_POST['password'];
