52324c81f7bb6d9a6aea78a9784b44682d44ed7f
svirekat <eketerinasv@gmail.com>
2025-05-01 10:43:33 +0300

    Update login.php

--- task5/login.php
+++ task5/login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Вход</title>
 </head>
 <body>
-    <form method="POST" action="">
+    <form method="POST" action="newindex.php">
         <label for="login">Логин:</label>
         <input type="text" id="login" name="login" required>
         <br>
