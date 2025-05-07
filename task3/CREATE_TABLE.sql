820f87044fbb55ec13e39e225f473ce6d0100507
svirekat <eketerinasv@gmail.com>
2025-05-07 19:27:00 +0300

    Update CREATE_TABLE.sql

--- task3/CREATE_TABLE.sql
+++ task3/CREATE_TABLE.sql
@@ -14,4 +14,4 @@ INSERT INTO langs VALUES (1, 'Pascal'), (2, 'C'), (3, 'C++'), (4, 'JavaScript'),
 (6, 'Python'), (7, 'Java'), (8, 'Haskell'), (9, 'Clojure'), (10, 'Prolog'), (11, 'Scala'), (12, 'Go');
 
 CREATE TABLE users_languages ( user_id INTEGER, lang_id INTEGER, PRIMARY KEY (user_id, lang_id), 
-FOREIGN KEY (user_id) REFERENCES users (user_id), FOREIGN KEY (lang_id) REFERENCES langs (lang_id) delete on cascade );
+FOREIGN KEY (user_id) REFERENCES users (user_id), FOREIGN KEY (lang_id) REFERENCES langs (lang_id) on delete cascade );
