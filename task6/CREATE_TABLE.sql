509fb5d5caba55924ab74d1e3ecd9a9d0296b4f6
svirekat <eketerinasv@gmail.com>
2025-05-23 01:09:43 +0300

    Update CREATE_TABLE.sql

--- task6/CREATE_TABLE.sql
+++ task6/CREATE_TABLE.sql
@@ -4,3 +4,7 @@ CREATE TABLE admins (
      login VARCHAR(50) NOT NULL UNIQUE,
      password VARCHAR(255) NOT NULL
 );
+
+INSERT INTO admins (fio, login, password) 
+     VALUES ('Свиридченко Екатерина', 'ekaterinasv', '$2y$10$nn34K.jwRfDMU4mH2ejY3OAQtzAJRNNT8J1g8iLD2zVbzTnGjiObK'
+);
