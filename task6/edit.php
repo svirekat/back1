d0fb203672ae4db3c0c0dfe4b900a06facdfa026
svirekat <eketerinasv@gmail.com>
2025-05-23 02:30:20 +0300

    Update edit.php

--- task6/edit.php
+++ task6/edit.php
@@ -10,7 +10,7 @@ if ($is_admin && isset($_GET['user_id'])) {
 } else {
     $user_id = $_SESSION['user_id']; 
 }
-$user_id = $_SESSION['user_id'];
+
 $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = ?");
 $stmt->execute([$user_id]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
