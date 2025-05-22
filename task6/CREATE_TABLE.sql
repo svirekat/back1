84349d7bfade12732352fb3a14ec8321d72257f4
svirekat <eketerinasv@gmail.com>
2025-05-22 22:10:20 +0300

    Create CREATE_TABLE.sql

---
+++ task6/CREATE_TABLE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE admins (
+     admin_id INT AUTO_INCREMENT PRIMARY KEY,
+     fio VARCHAR(150) NOT NULL,
+     login VARCHAR(50) NOT NULL UNIQUE,
+     password VARCHAR(255) NOT NULL
+);
