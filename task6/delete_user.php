10e6eb7ddc8873436cbcf163f7f9b1a09a441687
svirekat <eketerinasv@gmail.com>
2025-05-23 03:32:49 +0300

    Update delete_user.php

--- task6/delete_user.php
+++ task6/delete_user.php
@@ -6,7 +6,6 @@ $user_id = $_GET['user_id'];
 
 $stmt = $pdo->prepare("DELETE FROM users WHERE user_id = ?");
 $stmt->execute([$user_id]);
-echo "Пользователь успешно удалён.";
 
 header("Location: adminpage.php");
 exit();
